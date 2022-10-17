fd48198b683d466908345168661aa5dbca5848ff
Joao vitor santos <jvitor.spaula@gmail.com>
2022-10-17 15:51:00 -0300

    feat: finishing up the implement of methods abstract

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,26 +1,32 @@
 namespace DesafioPOO.Models
 {
-    public abstract class Smartphone
+  public abstract class Smartphone
+  {
+    public string Numero { get; set; }
+    // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+    private int Memoria { get; set; }
+    private string Modelo { get; set; }
+    private string Imei { get; set; }
+
+    public Smartphone(string numero, string modelo, string imei, int memoria)
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
-
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
-
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
-
-        public abstract void InstalarAplicativo(string nomeApp);
+      Numero = numero;
+      Memoria = memoria;
+      Modelo = modelo;
+      Imei = imei;
+      // TODO: Passar os parâmetros do construtor para as propriedades
     }
+
+    public void Ligar()
+    {
+      Console.WriteLine("Ligando...");
+    }
+
+    public void ReceberLigacao()
+    {
+      Console.WriteLine("Recebendo ligação...");
+    }
+
+    public abstract void InstalarAplicativo(string nomeApp);
+  }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone nokia:");
+Nokia nokia = new Nokia("123456", "Modelo 1", "11111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone("4987", "Modelo 2", "22222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
